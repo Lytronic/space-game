f92d166c0f49f0e336794c425146205fddaa4d60
FloofyDarkSpot <floofyshadow0@gmail.com>
2026-06-05 16:59:13 +0200

    Added the parts manager without any logic
    parts manager as child in the player scene that will soon manage all the upgrades to the ship

---
+++ scenes/player/PartsManager.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class PartsManager : Node2D
+{
+	// contains all the player stats here 
+	public override void _Ready()
+	{
+		//selects active and passive parts
+		//redefines player stats according to items
+		
+	}
+
+	
+	public override void _Process(double delta)
+	{
+		//calls the activate effect method of the children parts
+	}
+}
