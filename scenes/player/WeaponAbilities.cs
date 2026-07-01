2a215e010d5e79bb40706ad9dca08da41d68bd33
Indianajonas235 <jonasbernert@icloud.com>
2026-07-01 21:44:50 +0200

    Added particle folder as well as some textures and modified HUD to make it ready for the weapons gui

---
+++ scenes/player/WeaponAbilities.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class WeaponAbilities : HBoxContainer
+{
+	[Export] public Texture2D SpriteAbilityARC;
+	[Export] public Texture2D SpriteAbilityLASER;
+	[Export] public Texture2D SpriteAbilityMISSILE;
+	[Export] public Texture2D SpriteAbilityPLASMA;
+	[Export] public Texture2D SpriteAbilityEMP;
+	[Export] public Texture2D SpriteAbilityTORPEDO;
+	[Export] public Texture2D SpriteAbilityCANNON;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	// sample code for the abilities:
+	/*
+	
+	*/
+	
+}
