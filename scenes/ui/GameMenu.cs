3e51fcb4f4bd3794789d336eea992b390e786658
Indianajonas235 <jonasbernert@icloud.com>
2026-07-09 08:41:48 +0200

    Game Menu fixes and explosion particle fix Fixed Game Menu Resizing and changed explosion particle to be transparent

---
+++ scenes/ui/GameMenu.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class GameMenu : HBoxContainer
+{
+	private Node _soundManager;
+	public override void _Ready()
+	{
+		_soundManager = GetNode("/root/SoundManager");
+		GetNode<TextureButton>("VBoxContainerRight/TextureRect/SettingsButton").Pressed += () => {
+			_soundManager.Call("PlaySound", 0, 0);
+
+			SettingsScreen settings = (SettingsScreen)ResourceLoader.Load<PackedScene>("res://scenes/ui/SettingsScreen.tscn").Instantiate();
+			settings.Close += () => Visible = true; // restore visibility when settings close
+			GetNode<Node>("..").AddChild(settings);
+			Visible = false; // make this invisible
+		};
+		GetNode<TextureButton>("VBoxContainerRight/TextureRect/HighScoresButton").Pressed += () => {
+			_soundManager.Call("PlaySound", 0, 0);
+
+			HighScoresScreen settings = (HighScoresScreen)ResourceLoader.Load<PackedScene>("res://scenes/ui/HighScoresScreen.tscn").Instantiate();
+			settings.Close += () => Visible = true;
+			GetNode<Node>("..").AddChild(settings);
+			Visible = false;
+		};
+		GetNode<TextureButton>("VBoxContainerRight/TextureRect/QuitButton").Pressed += () => {
+			_soundManager.Call("PlaySound", 0, 0);
+			GetTree().Quit();
+		};
+		GetNode<TextureButton>("VBoxContainerLeft/BackButton").Pressed += () => {
+			_soundManager.Call("PlaySound", 0, 0);
+			GetTree().ChangeSceneToFile("res://scenes/ui/TitleScreen.tscn");
+		};
+	}
+
+
+	public override void _Process(double delta)
+	{
+
+	}
+}
